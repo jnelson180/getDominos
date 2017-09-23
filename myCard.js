f0b7be6d0c87eed82acafdde00b196f788703687
Jake Nelson <jnelson180@gmail.com>
2017-09-22 21:26:21 -0500

    Updated readme

--- myCard.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    cardNumber: "1111111111111111",
-    expiration: "1111",
-    securityCode: "111",
-    postalCode: "11111"
-}
--- myDetails.js
+++
@@ -1,9 +0,0 @@
-var homeAddress = require('./myAddress');
-
-module.exports = {
-    address: homeAddress,
-    firstName: "First",
-    lastName: "Last",
-    phone: "111-111-1111",
-    email: "111111+11@gmail.com"
-}
