4ff699c4d1b72dc5ab7a1b315b5202d45cc4a6f4
Jake Nelson <jnelson180@gmail.com>
2017-10-01 20:13:40 -0500

    removed order items to separate file in new /data directory

---
+++ data/items.js
@@ -0,0 +1,47 @@
+const pizzapi = require('pizzapi');
+
+// preconfigured orders/items
+
+module.exports = {
+    veg: new pizzapi.Item({
+        code: 'P14ITHPV',
+        options: [],
+        quantity: 1
+    }),
+
+    chz: new pizzapi.Item({
+        code: 'P14ITHCR',
+        options: [],
+        quantity: 1
+    }),
+
+    gButter: new pizzapi.Item({
+        code: 'GARBUTTER',
+        options: [],
+        quantity: 1
+    }),
+
+    ranch: new pizzapi.Item({
+        code: 'RANCH',
+        options: [],
+        quantity: 1
+    }),
+
+    hotWings: new pizzapi.Item({
+        code: 'W08PHOTW',
+        options: [],
+        quantity: 1
+    }),
+
+    coke: new pizzapi.Item({
+        code: '20BCOKE',
+        options: [],
+        quantity: 1
+    }),
+
+    cinnaBread: new pizzapi.Item({
+        code: 'B8PCCT',
+        options: [],
+        quantity: 1
+    })
+}
